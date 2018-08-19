9f5a866eeb43e77941576838d569ef8df768376d
Oran C <oranbusiness@gmail.com>
2018-08-19 15:10:07 -0700

    created renderImage function works!

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
-var img_data = [[]];
+var img_data = [[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1],
+[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1],
+[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1]];
 var img;
 
 function setup() {
   //setup
   createCanvas(600,600);
   background(50);
-  img = createImage(400,400);
+  // img = createImage(400,400);
  
   for(var x = 0; x < 0; x++) {
     for(var y = 0; y < 0; y++) {
@@ -20,15 +22,15 @@ function setup() {
   
   
   
-  img.loadPixels();
-  for(var x = 0; x < img.width; x++) {
-    for(var y = 0; y < img.height; y++) {
-      var r = map(y, 0, img.height, 255, 0);
-      img.set(x,y, [r,255-r,0,255]);
-    }   
-  }
-  img.updatePixels();
-
+  // img.loadPixels();
+  // for(var x = 0; x < img.width; x++) {
+  //   for(var y = 0; y < img.height; y++) {
+  //     var r = map(y, 0, img.height, 255, 0);
+  //     img.set(x,y, [r,255-r,0,255]);
+  //   }   
+  // }
+  // img.updatePixels();
+  img = renderImage(img_data,3,24);
 }
 
 function draw() {
@@ -44,4 +46,23 @@ function moveRobot(x,y){
 
 function readSerialVal(){
   //todo
+}
+//todo add heatmap
+//https://www.patrick-wied.at/static/heatmapjs/
+//datastruct object, width of datastruct, height of datastruct
+function renderImage(d, w,h){
+  var i = createImage(w,h);
+  i.loadPixels();
+  for(var x = 0; x < i.width; x++) {
+    for(var y = 0; y < i.height; y++) {
+      var r = map(y, 0, i.height, 255, 0);
+      //color
+      var c = d[x][y];
+      
+      //x,y position, r,g,b values, alpha(aka opasity)
+      i.set(x,y, [c, c,c,255]);
+    }   
+  }
+  i.updatePixels();
+  return i;
 }
