f1c0a9887894060c49ab7633b802d21562bb54f3
Oran C <oranbusiness@gmail.com>
2018-08-19 15:25:30 -0700

    loading of json file works

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 
+var serial;
+function preload() {
+  var dir =   'serial_data.json';
+  serial = loadJSON(dir);
+}
 
-var img_data = [[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1],
-[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1],
-[255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1,255,1]];
 var img;
 
 function setup() {
@@ -10,18 +12,18 @@ function setup() {
   createCanvas(600,600);
   background(50);
   
-  for(var x = 0; x < 0; x++) {
-    for(var y = 0; y < 0; y++) {
-  // for(var i = startx, endx)
-  // for(var j starty, endy)
-    moveRobot(x,y);
-    img_data[x][y] = readSerialVal();
-  // }
-  // }
-      }
-    }
-  
-  img = renderImage(img_data,3,24);
+  // for(var x = 0; x < 0; x++) {
+  //   for(var y = 0; y < 0; y++) {
+  // // for(var i = startx, endx)
+  // // for(var j starty, endy)
+  //   moveRobot(x,y);
+  //   img_data[x][y] = readSerialVal();
+  // // }
+  // // }
+  //     }
+  //   }
+    
+  img = imageRender(serial.data,3,24);
 }
 
 function draw() {
@@ -39,15 +41,21 @@ function readSerialVal(){
   //todo
 }
 
-//todo add heatmap
+//todo
+//[] add heatmap
+//[] add error checking if datastructure has ONLY 'numbers'
 //https://www.patrick-wied.at/static/heatmapjs/
+// demo
+// https://www.patrick-wied.at/static/heatmapjs/example-minimal-config.html
 //datastruct object, width of datastruct, height of datastruct
-function renderImage(d, w,h){
+function imageRender(d, w,h){
   var i = createImage(w,h);
   i.loadPixels();
   for(var x = 0; x < i.width; x++) {
     for(var y = 0; y < i.height; y++) {
       var r = map(y, 0, i.height, 255, 0);
+      
+      
       //color
       var c = d[x][y];
       
