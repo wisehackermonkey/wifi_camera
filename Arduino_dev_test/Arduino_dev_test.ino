7041399da497130e3961d51bbc4fbba87fc1c97d
Oran C <oranbusiness@gmail.com>
2018-08-19 15:42:32 -0700

    created arduino code to test datastructure imput

---
+++ Arduino_dev_test/Arduino_dev_test.ino
@@ -0,0 +1,23 @@
+/*by wisemonkey
+ * 
+*/
+
+const int sensorPin = A0;
+const int w = 100; 
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+//  if (Serial.available()>0) {
+    for(int i = 0; i <= w; i+=1){
+       int val = analogRead(sensorPin); 
+       Serial.print(val);
+       Serial.print(","); 
+       delay(100);   
+    }
+//  }
+}
+
+void loop() {
+  
+}
