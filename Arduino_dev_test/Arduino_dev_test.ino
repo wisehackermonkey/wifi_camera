07735758dd7317c4bc57225bc78408e1453c31dd
Oran C <oranbusiness@gmail.com>
2018-08-19 18:39:19 -0700

    changed arduino

--- Arduino_dev_test/Arduino_dev_test.ino
+++ Arduino_dev_test/Arduino_dev_test.ino
@@ -7,16 +7,17 @@ const int w = 100;
 void setup() {
   // initialize serial:
   Serial.begin(9600);
-  // reserve 200 bytes for the inputString:
-//  if (Serial.available()>0) {
-    for(int i = 0; i <= w; i+=1){
-       int val = analogRead(sensorPin); 
-       Serial.print(val);
-       Serial.print(","); 
-       delay(50);   
-    }
-    Serial.print("end");
-//  }
+  String output = "";
+  for(int i = 0; i <= w; i+=1){
+     int val = analogRead(sensorPin); 
+     output += String(val);
+     output += ","; 
+     delay(50);   
+  }
+
+  output += "end";
+  Serial.print(output);
+
 }
 
 void loop() {
